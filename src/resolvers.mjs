700c7c1b24d8970724a047a022a269e90b750c72
Marcin Delektowski <marcin.delektowski@gmail.com>
2022-11-20 18:35:33 +0100

    add old photo range query wip

--- src/resolvers.mjs
+++ src/resolvers.mjs
@@ -1,58 +1,59 @@
-import { setDateRange } from "./helpers.mjs";
+import {setDateRange} from "./helpers.mjs";
 import {
-  createPhotoData,
-  createSensorsData,
-  getAllSensorsData,
-  getDateRangeMeasurements,
-  getLastPhoto
+    createPhotoData,
+    createSensorsData,
+    getAllSensorsData,
+    getDateRangeMeasurements,
+    getLastPhoto, getOldPhotoFromRange
 } from "./sensorsData.mjs";
 
 async function saveMeasurements(temperature, humidity, pressure, measurementDate) {
-  const sensorData = {
-    temperature, humidity, pressure, measurementDate
-  };
-  await createSensorsData(sensorData);
+    const sensorData = {
+        temperature, humidity, pressure, measurementDate
+    };
+    await createSensorsData(sensorData);
 }
 
 async function savePhotoData(title, date) {
-  const photoData = {
-    title,
-    date
-  }
-  await createPhotoData(photoData);
+    const photoData = {
+        title,
+        date
+    };
+    await createPhotoData(photoData);
 }
 
 export const resolvers = {
-  Query: {
-    allMeasurements: async () => {
-      console.log(`Measurements has been queried on: ${new Date().toISOString()}`);
-      return getAllSensorsData();
-    }, dateRangeMeasurements: async (_, args) => {
-      return getDateRangeMeasurements(setDateRange(args.start, args.end));
-    }, lastPhoto: async () => {
-      return getLastPhoto();
-    }
-  },
-
-  Mutation: {
-    saveMeasurements: async (_, args) => {
-      args.measurementDate = new Date(args.measurementDate * 1000).toISOString();
-
-      await saveMeasurements(args.temperature, args.humidity, args.pressure, args.measurementDate);
-      console.log(`Measurement has been added on: ${args.measurementDate}`);
-      return {
-        code: 200, success: true, message: `Measurement has been added on: ${args.measurementDate}`
-      };
+    Query: {
+        allMeasurements: async () => {
+            console.log(`Measurements has been queried on: ${new Date().toISOString()}`);
+            return getAllSensorsData();
+        }, dateRangeMeasurements: async (_, args) => {
+            return getDateRangeMeasurements(setDateRange(args.start, args.end));
+        }, lastPhoto: async () => {
+            return getLastPhoto();
+        },
+        oldPhotoFromRange: async (_, args) => {
+            return getOldPhotoFromRange(args.start, args.end);
+        }
     },
-    savePhotoData: async (_, args) => {
-      args.date = new Date().toISOString();
 
-      await savePhotoData(args.title, args.date);
-      console.log(`Photo source has been saved on: ${args.date}`);
-      return {
-        code: 200, success: true, message: `Photo source has been saved on: ${args.date}`
-      };
+    Mutation: {
+        saveMeasurements: async (_, args) => {
+            args.measurementDate = new Date(args.measurementDate * 1000).toISOString();
+
+            await saveMeasurements(args.temperature, args.humidity, args.pressure, args.measurementDate);
+            console.log(`Measurement has been added on: ${args.measurementDate}`);
+            return {
+                code: 200, success: true, message: `Measurement has been added on: ${args.measurementDate}`
+            };
+        },
+        savePhotoData: async (_, args) => {
+            args.date = new Date().toISOString();
+            await savePhotoData(args.title, args.date);
+            console.log(`Photo source has been saved on: ${args.date}`);
+            return {
+                code: 200, success: true, message: `Photo source has been saved on: ${args.date}`
+            };
+        }
     }
-
-  }
 };
--- src/sensorsData.mjs
+++ src/sensorsData.mjs
@@ -1,24 +1,32 @@
-import { connectKnex } from "./knex.mjs";
+import {connectKnex} from "./knex.mjs";
 
 const tableMeasurements = "measurements";
 const tablePhoto = "photos";
 
 export function createSensorsData(sensorsData) {
-  return connectKnex(tableMeasurements).insert(sensorsData);
+    return connectKnex(tableMeasurements).insert(sensorsData);
 }
 
 export function createPhotoData(photoData) {
-  return connectKnex(tablePhoto).insert(photoData);
+    return connectKnex(tablePhoto).insert(photoData);
 }
 
 export function getAllSensorsData() {
-  return connectKnex(tableMeasurements).select("*");
+    return connectKnex(tableMeasurements).select("*");
 }
 
-export function getDateRangeMeasurements({ start, end }) {
-  return connectKnex(tableMeasurements).whereBetween("measurementDate", [start, end]);
+export function getDateRangeMeasurements({start, end}) {
+    return connectKnex(tableMeasurements).whereBetween("measurementDate", [start, end]);
 }
 
 export function getLastPhoto() {
-  return connectKnex(tablePhoto).select().orderBy([{column:"id",order:"desc"}]).limit(1);
+    return connectKnex(tablePhoto).select().orderBy([{column: "id", order: "desc"}]).limit(1);
+}
+
+export function getOldPhotoFromRange(start, end) {
+    console.log("start", start)
+    console.log("end", end)
+    const from = '2022-11-15T21:19:59.169Z';
+    const to = '2022-11-15T21:42:39.169Z';
+    return connectKnex(tablePhoto).select().whereBetween('date', [from, to]).limit(1);
 }
