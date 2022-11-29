845090be86608e424597b13bd6b12ec17a9c9da9
Marcin Delektowski <marcin.delektowski@gmail.com>
2022-11-30 00:10:32 +0100

    add remove old photos wip

--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,5 @@
 import {ApolloServer} from '@apollo/server';
-import { expressMiddleware } from '@apollo/server/express4';
+import {expressMiddleware} from '@apollo/server/express4';
 import express from 'express';
 import {typeDefs} from "./schema.mjs";
 import {resolvers} from "./resolvers.mjs";
@@ -9,21 +9,21 @@ import http from 'http';
 import https from 'https';
 import fs from 'fs';
 import * as path from 'path'
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 import * as dotenv from 'dotenv'
+import {handleRemoveOldPhotos} from "./removeOldPhotosData.mjs";
+
 dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const dir = path.join(__dirname, 'pics');
-console.log("dir", dir)
-const config =  { ssl: false, port: process.env.SERVER_PORT, hostname: process.env.SERVER_URL }
+const config = {ssl: false, port: process.env.SERVER_PORT, hostname: process.env.SERVER_URL}
 const server = new ApolloServer({
     typeDefs,
     resolvers,
 });
 await server.start();
-
 const app = express();
 
 app.use(express.static(dir));
@@ -44,7 +44,7 @@ if (config.ssl) {
     httpServer = http.createServer(app);
 }
 
-await new Promise((resolve) => httpServer.listen({ port: config.port }, resolve));
-
+await new Promise((resolve) => httpServer.listen({port: config.port}, resolve));
+handleRemoveOldPhotos()
 console.log('🚀 Server ready at', `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}`);
 
