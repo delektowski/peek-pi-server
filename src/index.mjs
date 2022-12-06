999335ecebd1448cbb066ef7c2f812bf5ba77fa5
Marcin Delektowski <marcin.delektowski@gmail.com>
2022-12-06 23:50:06 +0100

    add search for latest img recursively

--- src/index.mjs
+++ src/index.mjs
@@ -1,50 +1,57 @@
-import {ApolloServer} from '@apollo/server';
-import {expressMiddleware} from '@apollo/server/express4';
-import express from 'express';
-import {typeDefs} from "./schema.mjs";
-import {resolvers} from "./resolvers.mjs";
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import http from 'http';
-import https from 'https';
-import fs from 'fs';
-import * as path from 'path'
-import {fileURLToPath} from 'url';
-import * as dotenv from 'dotenv'
-import {handleRemoveOldPhotos} from "./removeOldPhotosData.mjs";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import { typeDefs } from "./schema.mjs";
+import { resolvers } from "./resolvers.mjs";
+import cors from "cors";
+import bodyParser from "body-parser";
+import http from "http";
+import https from "https";
+import fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from "url";
+import * as dotenv from "dotenv";
+import { handleRemoveOldPhotos } from "./removeOldPhotosData.mjs";
 
-dotenv.config()
+dotenv.config();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const dir = path.join(__dirname, 'pics');
-const config = {ssl: false, port: process.env.SERVER_PORT, hostname: process.env.SERVER_URL}
+const dir = path.join(__dirname, "pics");
+const config = {
+  ssl: false,
+  port: process.env.SERVER_PORT,
+  hostname: process.env.SERVER_URL,
+};
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+  typeDefs,
+  resolvers,
 });
 await server.start();
 const app = express();
-
 app.use(express.static(dir));
-app.use('', cors(), bodyParser.json(), expressMiddleware(server));
+app.use("", cors(), bodyParser.json(), expressMiddleware(server));
 
 let httpServer;
 
 if (config.ssl) {
-    httpServer = https.createServer(
-        {
-            key: fs.readFileSync(`./ssl/server.key`),
-            cert: fs.readFileSync(`./ssl/server.crt`),
-        },
+  httpServer = https.createServer(
+    {
+      key: fs.readFileSync(`./ssl/server.key`),
+      cert: fs.readFileSync(`./ssl/server.crt`),
+    },
 
-        app,
-    );
+    app
+  );
 } else {
-    httpServer = http.createServer(app);
+  httpServer = http.createServer(app);
 }
 
-await new Promise((resolve) => httpServer.listen({port: config.port}, resolve));
-handleRemoveOldPhotos()
-console.log('🚀 Server ready at', `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}`);
-
+await new Promise((resolve) =>
+  httpServer.listen({ port: config.port }, resolve)
+);
+handleRemoveOldPhotos();
+console.log(
+  "🚀 Server ready at",
+  `http${config.ssl ? "s" : ""}://${config.hostname}:${config.port}`
+);
--- src/sensorsData.mjs
+++ src/sensorsData.mjs
@@ -1,37 +1,57 @@
-import {connectKnex} from "./knex.mjs";
-import dayjs from 'dayjs'
+import { connectKnex } from "./knex.mjs";
+import dayjs from "dayjs";
 
 const tableMeasurements = "measurements";
 const tablePhoto = "photos";
 
 export function createSensorsData(sensorsData) {
-    return connectKnex(tableMeasurements).insert(sensorsData);
+  return connectKnex(tableMeasurements).insert(sensorsData);
 }
 
 export function createPhotoData(photoData) {
-    return connectKnex(tablePhoto).insert(photoData);
+  return connectKnex(tablePhoto).insert(photoData);
 }
 
 export function deletePhotoData(date) {
-    const formatDate = dayjs(date).format('YYYY-MM-DD')
-    return connectKnex(tablePhoto).where('title', 'like', `%${formatDate}%`).del()
+  const formatDate = dayjs(date).format("YYYY-MM-DD");
+  return connectKnex(tablePhoto)
+    .where("title", "like", `%${formatDate}%`)
+    .del();
 }
 
 export function getAllSensorsData() {
-    return connectKnex(tableMeasurements).select("*");
+  return connectKnex(tableMeasurements).select("*");
 }
 
-export function getDateRangeMeasurements({start, end}) {
-    return connectKnex(tableMeasurements).whereBetween("measurementDate", [start, end]);
+export function getDateRangeMeasurements({ start, end }) {
+  return connectKnex(tableMeasurements).whereBetween("measurementDate", [
+    start,
+    end,
+  ]);
 }
 
 export function getLastPhoto() {
-    return connectKnex(tablePhoto).select().orderBy([{column: "id", order: "desc"}]).limit(1);
+  return connectKnex(tablePhoto)
+    .select()
+    .orderBy([{ column: "id", order: "desc" }])
+    .limit(1);
 }
 
-export function getOldPhotoFromRange(start, end) {
-    console.log("start", start)
-    console.log("end", end)
+export async function getOldPhotoFromRange(start, end) {
+  let result = [];
+  let reqCount = 0;
 
-    return connectKnex(tablePhoto).select().whereBetween('date', [start, end]).limit(1);
+  async function getOldImg(start, end) {
+    result = await connectKnex(tablePhoto)
+      .select()
+      .whereBetween("date", [start, end])
+      .limit(1);
+    if (result.length === 0) {
+      start = dayjs(start).subtract(15, reqCount < 6000 ? "second" : "minute").toISOString();
+      return getOldImg(start, end);
+    }
+  }
+
+  await getOldImg(start, end);
+  return result;
 }
